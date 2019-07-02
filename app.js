c33612b6d8014f349ac31286fbef3a32d1834942
Vanessa <vanessayoshida92@gmail.com>
2019-07-02 13:53:12 -0300

    criando a rota padrao para API /

--- app.js
+++ app.js
@@ -5,9 +5,17 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true })); 
+app.get('/', function(req, res) {
+  res.json(
+    {
+      'name': 'burger-queen-api',
+      'version': '1.0.0'
+    }
+  )
+});
 app.use('/users', require('./routes/user'));
 app.use('/orders', require('./routes/orders'));
 app.use('/products', require('./routes/products'));
 
-app.listen(4967, console.log('Servidor rodando'));
+app.listen(8080, console.log('Servidor rodando'));
 db.sequelize.sync();
